e80512d13605b9ffa3aa49b465e21ed99351bb94
Liviu Iancu <lvudesign@gmail.com>
2016-05-01 12:41:25 +0300

    updated click functionality
    entire row is now clickable

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
     // set initial value to 0 if no box is checked, on first page load
     $('.total').text('0');
     // calculate total cost each time a checkbox is clicked
-    $('.clickarea').on('click', function() {
-        var clickable = $(this).find('.clickarea').attr("href");
-        if (clickable) {
-            window.location = href;
+    $('tr').on('click', function() {
+        // if the type attribute of the target element is not "checkbox."
+        if (event.target.type !== 'checkbox') {
+            // find all checkboxes within the clicked table row and click them
+            $(':checkbox', this).trigger('click');
         }
         var total = 0;
         // loop through each checked element and add to total
